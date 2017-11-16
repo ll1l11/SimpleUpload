c67fe70d88ce7c7d984937b8f5305215572be389
codeif <me@codeif.com>
2017-11-16 18:35:53 +0800

    version 0.0.2

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
+#!/usr/bin/env python
 from distutils.core import setup
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
 
 setup(
     name='SimpleUpload',
-    version='0.0.1',
+    version='0.0.2',
+    description='Upload file via HTTP.',
+    long_description=readme,
     packages=['simple_upload'],
     url='https://github.com/codeif/SimpleUpload',
     license='MIT',
     author='codeif',
     author_email='me@codeif.com',
-    description='Upload file via HTTP.',
     install_requires=['Flask']
 )
