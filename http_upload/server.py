3a804d63a0f9751fad8a3063c8d12f58b0b8e43c
codeif <me@codeif.com>
2017-11-16 11:28:26 +0800

    注释掉log配置，添加路径配置

--- http_upload/server.py
+++ http_upload/server.py
@@ -7,17 +7,22 @@ to run a Flask application with 4 worker processes (-w 4) binding to localhost p
 
     gunicorn -w 4 -b 127.0.0.1:4000 http_upload.server:app
 
+When source change, unicorn can add --reload option
+
 """
-import os.path
-import logging
-from logging.handlers import RotatingFileHandler
+# import os.path
+# import logging
+# from logging.handlers import RotatingFileHandler
 
 from flask import Flask, request
 from flask.views import MethodView
 
 
+# 可以把下面的配置写到配置文件
 DEBUG = False
 RANDOM_KEY = 'abc'
+CLIENT_PATH_PREFIX = ''
+SERVER_PATH_PREFIX = ''
 
 assert RANDOM_KEY
 
@@ -25,15 +30,17 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 app.config.from_envvar('HTTP_UPLOAD_SETTINGS', silent=True)
 
+assert app.config['RANDOM_KEY'] and app.config['CLIENT_PATH_PREFIX'] and app.config['SERVER_PATH_PREFIX']
+
 # log to file
-app.logger.setLevel(logging.INFO)
-log_path = os.path.join(os.path.dirname(__file__), 'web.log')
-handler = RotatingFileHandler(log_path, maxBytes=10*1024*1024, backupCount=1)
-handler.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s'
-                              '- %(message)s')
-handler.setFormatter(formatter)
-app.logger.addHandler(handler)
+# app.logger.setLevel(logging.INFO)
+# log_path = os.path.join(os.path.dirname(__file__), 'web.log')
+# handler = RotatingFileHandler(log_path, maxBytes=10*1024*1024, backupCount=1)
+# handler.setLevel(logging.INFO)
+# formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s'
+#                               '- %(message)s')
+# handler.setFormatter(formatter)
+# app.logger.addHandler(handler)
 
 
 @app.before_request
